aef590d0c00268bdadc781d548b8c0568291c850
Ayush Garg <ayush@science.org.in>
2023-11-12 02:08:05 -0500

    added

--- get_endpoint_html.py
+++ get_endpoint_html.py
@@ -19,10 +19,25 @@ def setup_driver():
     :return: The function `setup_driver()` returns a Chrome WebDriver instance.
     """
     options = Options()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    )
+    options.add_experimental_option(
+        "prefs",
+        {
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "plugins.always_open_pdf_externally": True,
+        },
+    )
 
-    driver = webdriver.Chrome(options=options)
+    chrome_driver_path = "/usr/bin/chromedriver"
+    service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
 
 
